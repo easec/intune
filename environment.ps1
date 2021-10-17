79f1745e89b258633fc0d13f1667f579bb3fbf1f
easec <mats.johannesson@easec.se>
2021-10-17 22:52:49 +0200

    Update environment.ps1

--- environment.ps1
+++ environment.ps1
@@ -7,9 +7,9 @@ New-ADOrganizationalUnit -Name "Lomma" -Path "DC=easec,DC=net"
 
 New-ADOrganizationalUnit -Name "Personal" -Path "OU=Lomma,DC=easec,DC=net"
 New-ADOrganizationalUnit -Name "Enheter" -Path "OU=Lomma,DC=easec,DC=net"
-New-ADOrganizationalUnit -Name "Klienter" -Path "OU=enheter,OU=Lomma,DC=easec,DC=net"
-New-ADOrganizationalUnit -Name "Servrar" -Path "OU=enheter,OU=Lomma,DC=easec,DC=net"
-New-ADOrganizationalUnit -Name "Mobila" -Path "OU=enheter,OU=Lomma,DC=easec,DC=net"
+New-ADOrganizationalUnit -Name "Klienter" -Path "OU=Enheter,OU=Lomma,DC=easec,DC=net"
+New-ADOrganizationalUnit -Name "Servrar" -Path "OU=Enheter,OU=Lomma,DC=easec,DC=net"
+New-ADOrganizationalUnit -Name "Mobila" -Path "OU=Enheter,OU=Lomma,DC=easec,DC=net"
 
 Write-Host "Skapar användare Gullan Holm, Karl Hult, Christer Larsson och Majken Larsson"
 Write-Host "Skapar användare Gullan Holm"
